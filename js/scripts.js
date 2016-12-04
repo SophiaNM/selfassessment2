b7f6b3dee79fc8a2b767f94966d69bb102b8d06a
SophiaNM <njerimurage@outlook.com>
2016-12-04 17:08:35 +0300

    Adding Navigation bar

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,16 @@ $('.Count').each(function () {
     }
   });
 });
+
+
+$(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);   
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
